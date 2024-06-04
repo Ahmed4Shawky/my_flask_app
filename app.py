4277a06f1a17d1b0545781dc88e7cd1ccd14e58f
Ahmed4Shawky <120585012+Ahmed4Shawky@users.noreply.github.com>
2024-06-04 21:34:18 +0300

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
 import torch
-import os
+
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -77,5 +77,5 @@ def health_check():
     return jsonify({"status": "OK"}), 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host="0.0.0.0", port=port)
+   
+    app.run(host="0.0.0.0", port=5000)
