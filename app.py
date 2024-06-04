5cbb5d69cd56bde1b252bbfa519ad4e91d51af24
Ahmed4Shawky <120585012+Ahmed4Shawky@users.noreply.github.com>
2024-06-05 02:06:22 +0300

    app.py

--- app.py
+++ app.py
@@ -1,37 +1,30 @@
 from flask import Flask, request, jsonify
 import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from scipy.special import softmax
-import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from model_loader import get_DistilBERT_analyzer
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Load NLTK's VADER
-nltk.download('vader_lexicon')
+nltk.download('vader_lexicon', download_dir='/tmp')
 sia = SentimentIntensityAnalyzer()
 
-# Load the transformer model and tokenizer (e.g., RoBERTa)
-tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
-model = AutoModelForSequenceClassification.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
+# Get the pre-loaded DistilBERT analyzer
+DistilBERT_analyzer = get_DistilBERT_analyzer()
 
 def analyze_sentiment(text):
     # VADER sentiment analysis
     vader_result = sia.polarity_scores(text)
 
-    # RoBERTa sentiment analysis
-    encoded_input = tokenizer(text, return_tensors='pt')
-    output = model(**encoded_input)
-    scores = output[0][0].detach().numpy()
-    scores = softmax(scores)
-    roberta_result = {
-        'roberta_neg': scores[0],
-        'roberta_neu': scores[1],
-        'roberta_pos': scores[2]
-    }
+    # DistilBERT sentiment analysis
+    DistilBERT_result = DistilBERT_analyzer(text)
+    DistilBERT_score = DistilBERT_result[0]['score']
 
-    return {**vader_result, **roberta_result}
+    return {
+        **vader_result,
+        'DistilBERT_score': DistilBERT_score
+    }
 
 def sentiment_to_stars(sentiment_score):
     thresholds = [0.2, 0.4, 0.6, 0.8]
@@ -51,7 +44,7 @@ def analyze():
     data = request.json
     text = data['text']
     sentiment_scores = analyze_sentiment(text)
-    star_rating = sentiment_to_stars(sentiment_scores['roberta_pos'])
+    star_rating = sentiment_to_stars(sentiment_scores['DistilBERT_score'])
     response = {
         'sentiment_scores': sentiment_scores,
         'star_rating': star_rating
