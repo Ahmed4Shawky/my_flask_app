259d0c87a16f7d47496f0ba325332dfd24b9aa4c
Ahmed4Shawky <120585012+Ahmed4Shawky@users.noreply.github.com>
2024-06-04 22:32:12 +0300

    app.py

--- app.py
+++ app.py
@@ -1,45 +1,47 @@
 from flask import Flask, request, jsonify
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 from scipy.special import softmax
 import torch
-
 
 # Initialize Flask app
 app = Flask(__name__)
 
+# Check if the VADER lexicon is already downloaded
+try:
+    nltk.data.find('sentiment/vader_lexicon.zip')
+except LookupError:
+    nltk.download('vader_lexicon')
+
 # Load NLTK's VADER lexicon once
-nltk.download('vader_lexicon')
 sia = SentimentIntensityAnalyzer()
 
-# Load the transformer model and tokenizer once
-tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
-model = AutoModelForSequenceClassification.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
+# Lazy load transformer model and tokenizer
+def get_transformer_pipeline():
+    tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
+    model = AutoModelForSequenceClassification.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
+    nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+    return nlp
 
 def analyze_sentiment(text):
     # VADER sentiment analysis
     vader_result = sia.polarity_scores(text)
 
     # RoBERTa sentiment analysis
-    encoded_input = tokenizer(text, return_tensors='pt')
-    
-    with torch.no_grad():
-        output = model(**encoded_input)
-    
-    scores = output[0][0].detach().numpy()
-    scores = softmax(scores)
+    nlp = get_transformer_pipeline()
+    roberta_result = nlp(text)[0]
 
-    # Convert numpy array to Python list (fix for TypeError)
-    scores_list = scores.tolist()
-
-    roberta_result = {
-        'roberta_neg': scores_list[0],
-        'roberta_neu': scores_list[1],
-        'roberta_pos': scores_list[2]
+    sentiment_scores = {
+        'vader_neg': vader_result['neg'],
+        'vader_neu': vader_result['neu'],
+        'vader_pos': vader_result['pos'],
+        'roberta_neg': roberta_result['score'] if roberta_result['label'] == 'LABEL_0' else 0,
+        'roberta_neu': roberta_result['score'] if roberta_result['label'] == 'LABEL_1' else 0,
+        'roberta_pos': roberta_result['score'] if roberta_result['label'] == 'LABEL_2' else 0
     }
 
-    return {**vader_result, **roberta_result}
+    return sentiment_scores
 
 def sentiment_to_stars(sentiment_score):
     thresholds = [0.2, 0.4, 0.6, 0.8]
@@ -77,5 +79,4 @@ def health_check():
     return jsonify({"status": "OK"}), 200
 
 if __name__ == "__main__":
-   
     app.run(host="0.0.0.0", port=5000)
