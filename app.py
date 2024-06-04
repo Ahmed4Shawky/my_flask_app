b382f2616a6ab607f827226b56cb37de5897ed5c
Ahmed4Shawky <120585012+Ahmed4Shawky@users.noreply.github.com>
2024-06-04 20:31:23 +0300

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
+import torch
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -11,17 +12,23 @@ app = Flask(__name__)
 nltk.download('vader_lexicon')
 sia = SentimentIntensityAnalyzer()
 
-# Load the transformer model and tokenizer (e.g., RoBERTa) outside of the request handlers
-tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
-model = AutoModelForSequenceClassification.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
+# Function to load the transformer model and tokenizer
+def load_transformer_model():
+    tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
+    model = AutoModelForSequenceClassification.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
+    return tokenizer, model
 
-def analyze_sentiment(text, tokenizer, model):
+def analyze_sentiment(text):
     # VADER sentiment analysis
     vader_result = sia.polarity_scores(text)
 
     # RoBERTa sentiment analysis
+    tokenizer, model = load_transformer_model()
     encoded_input = tokenizer(text, return_tensors='pt')
-    output = model(**encoded_input)
+    
+    with torch.no_grad():
+        output = model(**encoded_input)
+    
     scores = output[0][0].detach().numpy()
     scores = softmax(scores)
 
@@ -36,7 +43,6 @@ def analyze_sentiment(text, tokenizer, model):
 
     return {**vader_result, **roberta_result}
 
-
 def sentiment_to_stars(sentiment_score):
     thresholds = [0.2, 0.4, 0.6, 0.8]
     if sentiment_score <= thresholds[0]:
@@ -50,24 +56,15 @@ def sentiment_to_stars(sentiment_score):
     else:
         return 5
 
-
 @app.route('/analyze', methods=['POST'])
 def analyze():
     data = request.json
     text = data['text']
-    sentiment_scores = analyze_sentiment(text, tokenizer, model)
+    sentiment_scores = analyze_sentiment(text)
     star_rating = sentiment_to_stars(sentiment_scores['roberta_pos'])
 
     # Convert float32 values to standard float
-    sentiment_scores = {
-        'compound': float(sentiment_scores['compound']),
-        'neg': float(sentiment_scores['neg']),
-        'neu': float(sentiment_scores['neu']),
-        'pos': float(sentiment_scores['pos']),
-        'roberta_neg': float(sentiment_scores['roberta_neg']),
-        'roberta_neu': float(sentiment_scores['roberta_neu']),
-        'roberta_pos': float(sentiment_scores['roberta_pos'])
-    }
+    sentiment_scores = {k: float(v) for k, v in sentiment_scores.items()}
 
     response = {
         'sentiment_scores': sentiment_scores,
@@ -75,7 +72,6 @@ def analyze():
     }
 
     return jsonify(response)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
